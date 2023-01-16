82da52492fa56cc1f1e253d40cb39844c77f93aa
SantiagoTonda <superorbe@gmail.com>
2023-01-16 02:37:41 -0300

    Backend casi terminado

--- src/main/java/com/Porfolio/BackEnd/Entity/Persona.java
+++ src/main/java/com/Porfolio/BackEnd/Entity/Persona.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,6 +25,7 @@ public class Persona {
     @NotNull
     private String descripcion;
     
+    @NotBlank
     private String img;
 
     public Persona() {
